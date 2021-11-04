ab22404babc17cd428585c10734c8754448740ee
Rarysson Guilherme <r_guilherme12@hotmail.com>
2021-11-04 16:36:50 -0300

    Adicionado card de tarot

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,45 @@
 <template>
-  <h1>Tarot</h1>
+  <div class="cards-container">
+    <card
+      :card="tarot.cards[0]"
+      :frontCardUrl="tarot.imagesUrl"
+      :backCardImg="tarot.imageBackCard"
+    />
+  </div>
 </template>
 
 <script>
+import Card from "./components/Card.vue";
+
+import { tarot } from "./services/tarot";
+
 export default {
   name: "App",
+
+  components: {
+    Card,
+  },
+
+  data() {
+    return {
+      tarot,
+    };
+  },
 };
 </script>
+
+<style>
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  outline: none;
+}
+
+.cards-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(162px, 1fr));
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="card" :class="{ flipped: isFlipped }">
+    <div class="front"></div>
+    <div class="back" @click="isFlipped = true"></div>
+    <div class="info-container">
+      <p class="title">{{ card.name }}</p>
+      <p class="description">{{ card.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+
+  props: {
+    card: {
+      type: Object,
+      required: true,
+    },
+
+    frontCardUrl: {
+      type: String,
+      required: true,
+    },
+
+    backCardImg: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      isFlipped: false,
+    };
+  },
+
+  computed: {
+    bgImg() {
+      return `url(${this.backCardImg})`;
+    },
+
+    frontImg() {
+      return `url(${this.frontCardUrl}${this.card.image})`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transition: all 500ms;
+  transform-style: preserve-3d;
+  border-radius: 5px;
+  width: 162px;
+  height: 310px;
+}
+
+.front,
+.back {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  width: 100%;
+  height: 100%;
+}
+
+.front {
+  background-image: v-bind(frontImg);
+}
+
+.back {
+  cursor: pointer;
+  background-image: v-bind(bgImg);
+}
+
+.info-container {
+  position: absolute;
+  bottom: -30%;
+  opacity: 0;
+  transition: all 250ms;
+  transform: translateY(-100%);
+  pointer-events: none;
+}
+
+.info-container .title {
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.card.flipped {
+  transform: rotateY(180deg) scaleX(-1);
+  height: 341px;
+}
+
+.card.flipped .front {
+  border-radius: 0;
+  z-index: 1;
+}
+
+.card.flipped .back {
+  z-index: -1;
+}
+
+.card.flipped .info-container {
+  opacity: 1;
+  transform: translateY(0%);
+  pointer-events: all;
+}
+</style>
